b91faa1d6275a61bc1e4cb64d69d5d990f2d232b
Donkrzawayan <kadon991@gmail.com>
2023-01-14 20:13:31 +0100

    Bed prizes and rentals

---
+++ AccommodationApp/Controllers/RentalsController.cs
@@ -0,0 +1,31 @@
+﻿using DTO.InputDTOs;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using Services;
+
+namespace AccommodationApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RentalsController : Controller
+    {
+        private readonly IRentalService rentalService;
+
+        public RentalsController(IRentalService rentalService)
+        {
+            this.rentalService = rentalService;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<Rental?> GetAsync(int id)
+        {
+            return await rentalService.GetAsync(id);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync(RentalDTO rentalDTO)
+        {
+            return await rentalService.CreateAsync(rentalDTO);
+        }
+    }
+}
---
+++ Services/BedPrizeService.cs
@@ -0,0 +1,67 @@
+﻿using DTO.InputDTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace Services
+{
+    public class BedPrizeService : IBedPrizeService
+    {
+        private readonly IDbContextFactory<DatabaseContext> contextFactory;
+
+        public BedPrizeService(IDbContextFactory<DatabaseContext> contextFactory)
+        {
+            this.contextFactory = contextFactory;
+        }
+
+        public async Task<BedPrize?> GetAsync(int id)
+        {
+            using (DatabaseContext context = contextFactory.CreateDbContext())
+                return await context.BedPrizes.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<IActionResult> CreateAsync(BedPrizeDTO bedPrizeDto)
+        {
+            using (DatabaseContext context = contextFactory.CreateDbContext())
+            {
+                var apartment = await context.Apartments.FirstOrDefaultAsync(x => x.ID == bedPrizeDto.ApartmentId);
+
+                if (apartment == null)
+                    return new UnauthorizedObjectResult(bedPrizeDto.ApartmentId);
+
+                var bedPrice = await context.BedPrizes.AnyAsync(x => x.Id == bedPrizeDto.ApartmentId && x.BedsCount == bedPrizeDto.BedsCount);
+                if (bedPrice)
+                    return new UnprocessableEntityObjectResult("Such bed prize already exists.");
+
+                var newBedPrice = new BedPrize()
+                {
+                    BedsCount = bedPrizeDto.BedsCount,
+                    Prize = bedPrizeDto.Prize,
+                    Apartment = apartment
+                };
+
+                await context.BedPrizes.AddAsync(newBedPrice);
+                await context.SaveChangesAsync();
+            }
+            return new OkResult();
+        }
+
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            using (DatabaseContext context = contextFactory.CreateDbContext())
+            {
+                //TO DO: check if user that makes request has permission
+
+                var bedPrize = await context.BedPrizes.FirstOrDefaultAsync(x => x.Id == id);
+
+                if (bedPrize == null)
+                    return new UnprocessableEntityObjectResult("Bed prize does not exist.");
+
+                context.BedPrizes.Remove(bedPrize);
+
+                await context.SaveChangesAsync();
+            }
+            return new OkResult();
+        }
+    }
+}
